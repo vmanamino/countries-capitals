76a7b0703c4a7ffb02222fc49d508db2606838ca
vmanamino <vmanamino@gmail.com>
2016-05-08 21:52:37 +0000

    basic functionality

--- app/cc-app.js
+++ app/cc-app.js
@@ -29,6 +29,7 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate', 'ngRoute'])
         pop: '',
         area: '',
         capital: '',
+        code: '',
         capPop: '',
         numNeigh: 0,
         neighbors: []
@@ -48,6 +49,15 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate', 'ngRoute'])
       params: request
     });
   }])
+  .factory('getCountry', ['$http', function($http){
+    return function(countryCode){
+       return $http({
+         method: 'GET',
+         url: 'http://api.geonames.org/countryInfoJSON',
+         params: { username: 'vman', country: countryCode }
+       });
+    };
+  }])
   .factory('getNeighbors', ['$http', function($http){
     return function(geonameId){
       return $http({
@@ -56,6 +66,17 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate', 'ngRoute'])
         params: { username: 'vman', geonameId: geonameId }
       });
     };
+  }])
+  .factory('buildCountry', ['countryInfo', function(countryInfo){
+      return function(geocountry, response){
+          countryInfo.name = geocountry.countryName;
+          countryInfo.code = geocountry.countryCode;
+          countryInfo.pop = geocountry.population;
+          countryInfo.area = geocountry.areaInSqKm;
+          countryInfo.capital = geocountry.capital;
+          countryInfo.neighbors = response.data.geonames;
+          countryInfo.numNeigh = response.data.geonames.length;
+      };
   }])
   // .run(['$rootScope', '$location', function($rootScope, $location) {
   //       $rootScope.$on('$routeChangeError', function() {
@@ -79,21 +100,22 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate', 'ngRoute'])
           }
       }; 
   }])
-  .controller('CountriesCtrl', ['$scope', '$route', '$routeParams', '$location','countryInfo', 'countriesCache', 'getNeighbors', 
-      function($scope, $route, $routeParams, $location, countryInfo, countriesCache, getNeighbors){
+  .controller('CountriesCtrl', ['$scope', '$routeParams', '$location','countryInfo', 'countriesCache', 'getNeighbors', 'buildCountry',
+      function($scope, $routeParams, $location, countryInfo, countriesCache, getNeighbors, buildCountry){
       var cache = countriesCache;
       $scope.geocountries = cache.get('called');
       console.log("above func");
       console.log($routeParams);
       $scope.toCountry = function(geocountry){
         getNeighbors(geocountry.geonameId).then(function(response){
-            countryInfo.name = geocountry.countryName;
-            countryInfo.pop = geocountry.population;
-            countryInfo.area = geocountry.areaInSqKm;
-            countryInfo.capital = geocountry.capital;
-            countryInfo.neighbors = response.data.geonames;
-            countryInfo.numNeigh = response.data.geonames.length;
-            console.log(countryInfo.neighbors);
+            buildCountry(geocountry, response);
+            // countryInfo.name = geocountry.countryName;
+            // countryInfo.pop = geocountry.population;
+            // countryInfo.area = geocountry.areaInSqKm;
+            // countryInfo.capital = geocountry.capital;
+            // countryInfo.neighbors = response.data.geonames;
+            // countryInfo.numNeigh = response.data.geonames.length;
+            // console.log(countryInfo.neighbors);
         }),
         function(response){
           alert('Error');
@@ -105,11 +127,32 @@ angular.module('ccApp', ['ngRoute', 'ngAnimate', 'ngRoute'])
       console.log("countries path");
       console.log($routeParams);
   }])
-  .controller('CountryCtrl', ['$scope', '$location', 'countryInfo', 'getNeighbors', '$routeParams',
-      function($scope, $location, countryInfo, getNeighbors, $routeParams){
-      // $location.path('/countries/'+countryInfo.name+'/capital');
-      console.log("country views");
-      console.log($routeParams);
+  .controller('CountryCtrl', ['$scope', '$location', 'countryInfo', 'getNeighbors', 'getCountry', 'buildCountry', '$routeParams',
+      function($scope, $location, countryInfo, getNeighbors, getCountry, buildCountry, $routeParams){
       $scope.country = countryInfo;
+      var geocountry = '';
+      $scope.toCountry = function(neighbor){
+        console.log("geocountry neighbor");
+        console.log(neighbor);
+        getCountry(neighbor.countryCode).then(function(response){
+            geocountry = response.data.geonames[0];
+        }),
+        function(response){
+          alert("error");
+        };
+        getNeighbors(neighbor.geonameId).then(function(response){
+            buildCountry(geocountry, response);
+            $scope.country = countryInfo;
+        }),
+        function(response){
+          alert('Error');
+        };
+      };
+      $scope.goHome = function(){
+        $location.path('/');
+      };
+      $scope.countries = function(){
+        $location.path('/countries');
+      };
   }]);
   
