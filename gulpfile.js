c3f379674439472e26c16f730fa00bda99ee122d
vmanamino <vmanamino@gmail.com>
2016-05-13 04:38:44 +0000

    build dir created

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,23 @@ gulp.task('connect', function() {
     root: 'app/'
   });
 });
+
+gulp.task('copy-html-files', function() {
+  gulp.src(['./app/**/*.html', '!./app/index.html'], {base: './app'})
+  .pipe(gulp.dest('build/'));
+});
+
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['copy-html-files', 'usemin']);
