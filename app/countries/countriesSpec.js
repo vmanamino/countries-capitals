2026e2bbd69e22e3054c633d975688892869188b
vmanamino <vmanamino@gmail.com>
2016-05-20 04:19:17 +0000

    modified controller loading

--- app/countries/countriesSpec.js
+++ app/countries/countriesSpec.js
@@ -13,28 +13,29 @@ describe('ccApp', function(){
         beforeEach(inject(function($controller, $rootScope, $location, $httpBackend, getCountries){
             httpBackend = $httpBackend;
             getLaender = getCountries;
+            var neighbors = {
+                "data": { "geonames": [{"name": "liberland"}, {"name" : "nueva espana"}] }
+            };
+            httpBackend.expect('GET', 'http://api.geonames.org/countryInfoJSON?username=vman').respond(neighbors);
+            status = false;
+            getLaender.countriesObject.then(function(){
+                status = true;
+            });
             location = $location;
             scope = $rootScope.$new();
             ctrl = $controller('CountriesCtrl', {
                 $scope: scope
             });
         }));
-        // it('should getCountries', function(){
-        //     var neighbors = {
-        //         "data": { "geonames": [{"name": "liberland"}, {"name" : "nueva espana"}] }
-        //     };
-        //     httpBackend.expect('GET', 'http://api.geonames.org/countryInfoJSON?username=vman').respond(neighbors);
-        //     status = false;
-        //     getLaender.countriesObject.then(function(){
-        //         status = true;
-        //     });
-        //     // scope.geocountries = 'countriesGot';
-        //     scope.$digest();
-        //     httpBackend.flush();
-        //     expect(status).toBe(true);
-        //     // expect(scope.geocountries.length).toBe(2);
-        //     httpBackend.verifyNoOutstandingRequest();
-        // })
+        it('should getCountries', function(){
+            // scope.geocountries = 'countriesGot';
+            scope.$digest();
+            httpBackend.flush();
+            expect(status).toBe(true);
+            // expect(scope.geocountries.length).toBe(2);
+            httpBackend.verifyNoOutstandingRequest();
+            httpBackend.verifyNoOutstandingExpectation();
+        })
         it('should go to root via goHome vunction', function(){
             scope.goHome();
             expect(location.path()).toEqual('/');
